c8b854e504443fc2b222b84d31438429a13e3f2a
Pawel Bugno (PGS Software) <pbugno@pgs-soft.com>
2020-04-25 23:09:48 +0200

    [TASK1] Do cleanup

--- src/arena.py
+++ src/arena.py
@@ -1,19 +1,19 @@
-WORKS = True
-
 from combatants import Combatant
 from util import json_operations
 from random import randint
 
-class Arena:
-    def __create_combatants(self, path='../tasks/combat/combatants.json'):
-        elements = json_operations.read_from_file(path)
-        result = []
-        for e in elements:
-            result.append(Combatant(**e))
-        return result
 
-    def __init_round(self):
-        combatants = self.__create_combatants()
+def create_combatants(path='../tasks/combat/combatants.json'):
+    elements = json_operations.read_from_file(path)
+    result = []
+    for e in elements:
+        result.append(Combatant(**e))
+    return result
+
+
+class Arena:
+    def __init__(self):
+        combatants = create_combatants()
         self.attacker = combatants[randint(0, 1)]
         combatants.remove(self.attacker)
         self.opponent = combatants[0]
@@ -26,19 +26,6 @@ class Arena:
         return None
 
     def start_battle(self):
-        # combatants = self.create_combatants()
-        # attacker_index = randint(0,1)
-        # attacker = combatants[attacker_index]
-        # combatants.remove(attacker)
-        # opponent = combatants[0]
-
-        # current_attacker.opponent = opponent
-        # opponent.opponent = attacker
-        # print(str(type(attacker)))
-        # print(str(type(opponent)))
-
-        # attacker.
-        self.__init_round()
         round_number = 1
         while True:
             self.attacker.attack(self.opponent)
@@ -51,27 +38,7 @@ class Arena:
             self.opponent = self.attacker.opponent
         print('{} wins in {} rounds!'.format(winner.name, round_number))
 
-# c = Arena().start_battle()
+
 Arena().start_battle()
 
-# print(str(type(c[0])))
-
-# t = ['a','b']
-# print(len(t[0]))
-#
-# combatants = Arena().create_combatants()
-# attacker_index = randint(0,1)
-# # attacker = combatants[int(attacker_index)]
-# attacker = combatants[attacker_index]
-# print(attacker.name)
-# combatants.remove(attacker)
-# print(combatants[0].name)
-# # attacker.is_won()
-# # attacker = combatants[0]
-# # print(type(randint(0,1)))
-#
-# # Arena().start_battle()
-# print('### {}',hasattr(Arena(), 'start_battle'))
-# a= Arena()
-# # a.x=1
-# # print((hasattr(a, 'x') & a.x > 0))
+WORKS = True
--- src/combatants.py
+++ src/combatants.py
@@ -1,20 +1,19 @@
 import re
-from util import json_operations
 from random import randint
 
 
 class DiceSimulator:
     def encode(self, expr: str):
-        prefix_pattern = '(\d+)d(\d+)'
-        suffix_pattern = '\s+\+\s+(\d+)'
+        prefix_pattern = r'(\d+)d(\d+)'
+        suffix_pattern = r'\s+\+\s+(\d+)'
         pattern = prefix_pattern + suffix_pattern
         x = 0
         y = 0
         z = 0
-        if (re.match(pattern, expr)):
+        if re.match(pattern, expr):
             z = int(re.search(pattern, expr).group(3))
         pattern = prefix_pattern
-        if (re.match(pattern, expr)):
+        if re.match(pattern, expr):
             x = re.search(pattern, expr).group(1)
             y = re.search(pattern, expr).group(2)
         self.__init(int(x), int(y), int(z))
@@ -55,26 +54,3 @@ class Combatant(object):
     def print_round(self, round_number: int):
         print('{} {} {} {} {} {}'.format(round_number, self.name, self.opponent.name, self.damage,
                                          self.opponent.hp_before_attach, self.opponent.hp_after_attack))
-
-
-def create_combatants(path = '../tasks/combat/combatants.json'):
-    elements = json_operations.read_from_file(path)
-    result = []
-    for e in elements:
-        result.append(Combatant(**e))
-    return result
-
-
-# print(create_combatants('../tasks/combat/combatants.json')[0].random_damage())
-# print(create_combatants('../tasks/combat/combatants.json')[0])
-# print(create_combatants()[0])
-
-# combatants = create_combatants()
-# c1 = combatants[0]
-# c2 = combatants[1]
-# c1.attack(c2)
-# c1.print_round(1)
-# c1.attack(c2)
-# c1.print_round(2)
-#
-# print('{} - {}'.format(1, 'a'))
