7b6c03827d9f78e6a84333933a714b97abcae286
Pawel Bugno (PGS Software) <pbugno@pgs-soft.com>
2020-04-27 12:29:52 +0200

    [TASK1] Remove 'opponent' from Combatant class

--- src/arena.py
+++ src/arena.py
@@ -20,19 +20,17 @@ class Arena:
         self.attacker = combatants[randint(0, 1)]
         combatants.remove(self.attacker)
         self.opponent = combatants[0]
-        self.attacker.opponent = self.opponent
-        self.opponent.opponent = self.attacker
 
     def get_winner(self) -> Combatant:
-        if self.attacker.is_winner():
-            return self.attacker
-        if self.opponent.is_winner():
+        if self.attacker.is_lost():
             return self.opponent
+        if self.opponent.is_lost():
+            return self.attacker
         return None
 
-    def swap_combatants(self) -> None:
-        self.attacker = self.attacker.opponent
-        self.opponent = self.attacker.opponent
+    def print_round(self, round_number: int) -> None:
+        print(f'{round_number} {self.attacker.name} {self.opponent.name} {self.attacker.damage} '
+              f'{self.opponent.hp_before_attack} {self.opponent.hp_after_attack}')
 
     def start_battle(self):
         round_number = 0
@@ -40,9 +38,9 @@ class Arena:
         while not winner:
             round_number += 1
             self.attacker.attack(self.opponent)
-            self.attacker.print_round(round_number)
+            self.print_round(round_number)
             winner = self.get_winner()
-            self.swap_combatants()
+            self.attacker, self.opponent = self.opponent, self.attacker
         print(f'{winner.name} wins in {round_number} rounds!')
 
 
