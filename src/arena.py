a08dabbd2db5fe668874bb7952f79f0106ce3c6c
mstandio <mstandio@o2.pl>
2020-04-28 11:55:21 +0200

    mstandio

--- src/arena.py
+++ src/arena.py
@@ -1,26 +1,26 @@
 import json
 import re
 import random
+
 import falcon
 from wsgiref import simple_server
 from typing import List
 from json import JSONEncoder
 
+from dataclasses import dataclass
+
 
 class Combatant:
-    name: str
-    last_damage: int
-    last_health: int
-    health: int
-    __damage: 'Damage'
 
     def __init__(self, name: str, hp: int, damage: str):
         self.name = name
         self.health = hp
         self.__damage = self.Damage(damage)
+        self.last_damage = 0
+        self.last_health = hp
 
-    def attack(self,  other: 'Combatant'):
-        self.last_damage = self.__damage.draw();
+    def attack(self, other: 'Combatant'):
+        self.last_damage = self.__damage.draw()
         other.__get_attacked(self.last_damage)
 
     def __get_attacked(self, damage: int):
@@ -33,75 +33,31 @@ class Combatant:
         return self.health == 0
 
     class Damage:
-        __x: int
-        __y: int
-        __z: int
+        __dice_roll_number: int
+        __dice_sides_number: int
+        __attack_bonus: int
 
         def __init__(self, damage: str):
             # XdY[ + Z]
             m = re.search(r"^(\d+)d(\d+)( \+ (\d+))?$", damage)
             if m:
-                self.__x = int(m.group(1))
-                self.__y = int(m.group(2))
+                self.__dice_roll_number = int(m.group(1))
+                self.__dice_sides_number = int(m.group(2))
                 z = m.group(4)
                 if z:
-                    self.__z = int(z)
+                    self.__attack_bonus = int(z)
                 else:
-                    self.__z = 0
+                    self.__attack_bonus = 0
 
         def draw(self):
             result = 0
-            for i in range(self.__x):
-                result += random.randint(1, self.__y)
-            result += self.__z
+            for i in range(self.__dice_roll_number):
+                result += random.randint(1, self.__dice_sides_number)
+            result += self.__attack_bonus
             return result
 
 
 Combatants = List[Combatant]
-
-
-class Round:
-    round_number: str
-    attacker: str
-    defender: str
-    damage: int
-    previous_hp: int
-    current_hp: int
-
-    def __init__(self, round_number: str,
-                 attacker: str, defender: str, damage: int,
-                 previous_hp: int, current_hp: int):
-        self.round_number = round_number
-        self.attacker = attacker
-        self.defender = defender
-        self.damage = damage
-        self.previous_hp = previous_hp
-        self.current_hp = current_hp
-
-
-Rounds = List[Round]
-
-
-class Victory:
-    winner: str
-    rounds: int
-    ko: bool
-
-    def __init__(self, winner: str,
-                 rounds: int,
-                 ko: bool):
-        self.winner = winner
-        self.rounds = rounds
-        self.ko = ko
-
-
-class Result:
-    rounds: Rounds
-    victory: Victory
-
-    def __init__(self, rounds: Rounds, victory: Victory):
-        self.rounds = rounds
-        self.victory = victory
 
 
 class Arena:
@@ -114,9 +70,9 @@ class Arena:
         self.c2 = c2
         self.max_rounds = max_rounds
 
-    def fight(self) -> Result:
-        rounds: Rounds = []
-        victory: Victory
+    def fight(self) -> 'BattleResult':
+        rounds: 'BattleResult.Rounds' = []
+        victory: 'BattleResult.Victory'
         round_number: int = 1
         while not self.c1.is_dead() and not self.c1.is_dead() and round_number <= self.max_rounds:
             rounds.append(self.next_round(round_number, self.c1, self.c2))
@@ -124,29 +80,45 @@ class Arena:
             round_number += 1
         else:
             if self.c1.health > self.c2.health:
-                print('{0} won!'.format(self.c1.name))
-                victory = Victory(self.c1.name, round_number - 1, self.c2.is_dead())
+                victory = self.BattleResult.Victory(self.c1.name, round_number - 1, self.c2.is_dead())
             else:
-                print('{0} won!'.format(self.c2.name))
-                victory = Victory(self.c2.name, round_number - 1, self.c1.is_dead())
-        return Result(rounds, victory)
+                victory = self.BattleResult.Victory(self.c2.name, round_number - 1, self.c1.is_dead())
+        print(f'{victory.winner} won!')
+        return self.BattleResult(rounds, victory)
 
-    @staticmethod
-    def next_round(round_number: int, attacker: Combatant, defender: Combatant) -> Round:
+    def next_round(self, round_number: int, attacker: Combatant, defender: Combatant) -> 'Round':
         attacker.attack(defender)
-        print('{0} {1} {2} {3} {4} {5}' \
-              .format(str(round_number),
-                      attacker.name,
-                      defender.name,
-                      str(attacker.last_damage),
-                      str(defender.last_health),
-                      str(defender.health)))
-        return Round(round_number,
-                     attacker.name,
-                     defender.name,
-                     attacker.last_damage,
-                     defender.last_health,
-                     defender.health)
+        print(f'{str(round_number)} {attacker.name} {defender.name} {str(attacker.last_damage)} '
+              f'{str(defender.last_health)} {str(defender.health)}')
+        return self.BattleResult.Round(round_number,
+                                       attacker.name,
+                                       defender.name,
+                                       attacker.last_damage,
+                                       defender.last_health,
+                                       defender.health)
+
+    @dataclass
+    class BattleResult:
+
+        rounds: 'Rounds'
+        victory: 'Victory'
+
+        @dataclass
+        class Round:
+            round_number: str
+            attacker: str
+            defender: str
+            damage: int
+            previous_hp: int
+            current_hp: int
+
+        Rounds = List['Battle.Round']
+
+        @dataclass
+        class Victory:
+            winner: str
+            rounds: int
+            ko: bool
 
 
 def load_combatants() -> Combatants:
@@ -159,7 +131,7 @@ def combatant_decoder(obj) -> Combatant:
     return Combatant(str(obj['name']), int(obj['hp']), str(obj['damage']))
 
 
-def main() -> Result:
+def main() -> Arena.BattleResult:
     combatants: Combatants = load_combatants()
     return Arena(combatants[0], combatants[1], 10).fight()
 
@@ -173,8 +145,7 @@ class ThingsResource(object):
 
     def on_post(self, req, resp):
         resp.status = falcon.HTTP_200
-        result: Result = main()
-        resp.body = json.dumps(result, cls=MyEncoder)
+        resp.body = json.dumps(main(), cls=MyEncoder)
 
 
 app = falcon.API()
--- tests/test_arena.py
+++ tests/test_arena.py
@@ -1,36 +1,66 @@
-import arena
 from unittest import TestCase
 
-
-class TestDamage(TestCase):
-
-    def test_damage_parsing_success(self):
-        damage1 = arena.Damage("1d2")
-        self.assertTrue(damage1._Damage__x == 1)
-        self.assertTrue(damage1._Damage__y == 2)
-        self.assertTrue(damage1._Damage__z == 0)
-
-        damage2 = arena.Damage("1d2 + 3")
-        self.assertTrue(damage2._Damage__x == 1)
-        self.assertTrue(damage2._Damage__y == 2)
-        self.assertTrue(damage2._Damage__z == 3)
-        pass
-
-    def test_damage_drawing(self):
-        damage = arena.Damage("1d2 + 3")
-        self.assertTrue(damage.draw() > 0)
-        pass
+import arena
 
 
 class TestCombatant(TestCase):
 
+    def test_damage_parsing_success(self):
+        damage1 = arena.Combatant.Damage("1d2")
+        self.assertEqual(damage1._Damage__dice_roll_number, 1)
+        self.assertEqual(damage1._Damage__dice_sides_number, 2)
+        self.assertEqual(damage1._Damage__attack_bonus, 0)
+
+        damage2 = arena.Combatant.Damage("1d2 + 3")
+        self.assertEqual(damage2._Damage__dice_roll_number, 1)
+        self.assertEqual(damage2._Damage__dice_sides_number, 2)
+        self.assertEqual(damage2._Damage__attack_bonus, 3)
+
+    def test_damage_drawing(self):
+        arena.random.randint = lambda a, b: 2
+        self.assertEqual(arena.Combatant.Damage("1d2 + 3").draw(), 5)
+
     def test_combatant(self):
-        combatant = arena.Combatant("c", 100, arena.Damage("1d2"))
-        self.assertFalse(combatant.is_dead())
+        arena.random.randint = lambda a, b: 3
 
-        last_damage = combatant.attack()
-        self.assertTrue(last_damage == combatant.last_damage)
+        attacker = arena.Combatant("a", 100, "1d2")
+        defender = arena.Combatant("b", 5, "1d2")
 
-        last_health = combatant.health
-        self.assertTrue(combatant.get_attacked(100) == 0)
-        self.assertTrue(last_health == combatant.last_health)
+        self.assertEqual(5, defender.last_health)
+        self.assertEqual(5, defender.health)
+        self.assertFalse(defender.is_dead())
+
+        attacker.attack(defender)
+        self.assertEqual(3, attacker.last_damage)
+        self.assertEqual(5, defender.last_health)
+        self.assertEqual(2, defender.health)
+        self.assertFalse(defender.is_dead())
+
+        attacker.attack(defender)
+        self.assertEqual(3, attacker.last_damage)
+        self.assertEqual(2, defender.last_health)
+        self.assertEqual(0, defender.health)
+        self.assertTrue(defender.is_dead())
+
+
+class TestArena(TestCase):
+
+    def test_fight(self):
+        arena.random.randint = lambda a, b: 3
+        battle_result_1: arena.Arena.BattleResult = arena.Arena(
+            arena.Combatant("a", 100, "1d2"),
+            arena.Combatant("b", 5, "1d2"),
+            1).fight()
+
+        victory_1: arena.Arena.BattleResult.Victory = battle_result_1.victory
+        self.assertEqual("a", victory_1.winner)
+        self.assertEqual(1, victory_1.rounds)
+        self.assertFalse(victory_1.ko)
+
+        self.assertEqual(1, len(battle_result_1.rounds))
+        round_1: arena.Arena.BattleResult.Round = battle_result_1.rounds[0]
+        self.assertEqual(1, round_1.round_number)
+        self.assertEqual("a", round_1.attacker)
+        self.assertEqual("b", round_1.defender)
+        self.assertEqual(5, round_1.previous_hp)
+        self.assertEqual(2, round_1.current_hp)
